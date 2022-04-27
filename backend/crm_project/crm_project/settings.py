d4182fdc51865a81e36b8ce549a924258e1498e3
aviatorer <itsjamesteo@gmail.com>
2022-04-27 16:51:45 +0800

    Setup account folder, account models, setup db settings
    Installed psycopg2
    Set up account folder
    Created user models
    Created database
    Created superuser

--- backend/crm_project/crm_project/settings.py
+++ backend/crm_project/crm_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'crm_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'crm_database',
+        'USER': 'test_user',
+        'PASSWORD': 'example',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
