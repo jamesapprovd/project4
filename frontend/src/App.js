098b9b06e9fa4d4d99ae2dc932cf319c47d6d74d
aviatorer <itsjamesteo@gmail.com>
2022-04-29 15:38:00 +0800

    Setup react app and HTML
    Installed react node modules
    Designed company logo and favicon
    Amended HTML
    Amended App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h2>RoboCorp</h2>
+      <img src="https://i.imgur.com/zcwGl5R.png?1" alt="RoboCorp Logo"></img>
+    </div>
+  );
+}
+
+export default App;
